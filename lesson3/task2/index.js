666997b37cda2fb1fdd83288dbbde2807256d7e4
liubov4 <liub.chernova@gmail.com>
2019-11-03 13:56:52 +0200

    new task

---
+++ lesson3/task2/index.js
@@ -0,0 +1,48 @@
+17
+console.log('17 to string is ' + String(17)); -
+17.17
+console.log('-17.17 to string is ' + String(-17.17));
+false
+console.log('false to string is ' + String(false));
+null
+console.log('null to string is ' + String(null));
+undefined
+console.log('undefined to string is ' + String(undefined));
+0
+console.log('0 to string is ' + String(0));
+
+'17'
+console.log("'17' to number is " + Number('17'));
+true
+console.log("true to number is " + Number(true));
+false
+console.log("false to number is " + Number(false));
+null
+console.log("null to number is " + Number(null));
+undefined
+console.log("undefined to number is " + Number(undefined));
+'   20   '
+console.log("'   20   ' to number is " + Number('   20   '));
+'      '
+console.log("'      ' to number is " + Number('      '));
+'   30d   '
+console.log("'   30d   ' to number is " + Number('   30d   '));
+
+null
+console.log('null to boolean is ' + Boolean('null'));
+undefined
+console.log('undefined to boolean is ' + Boolean('undefined'));
+0
+console.log('0 to boolean is ' + Boolean('0')); -
+0
+console.log('-0 to boolean is ' + Boolean('-0'));
+NaN
+console.log('NaN to boolean is ' + Boolean('NaN'));
+''
+console.log("'' to boolean is " + Boolean(''));
+' '
+console.log("' ' to boolean is " + Boolean(' '));
+17
+console.log('17 to boolean is ' + Boolean('17'));
+'Hello'
+console.log("'Hello' to boolean is " + Boolean("'Hello'"));
