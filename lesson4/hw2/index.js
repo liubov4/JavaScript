5790cf34dd08b3f382825baea09ab275346cce78
liubov4 <liub.chernova@gmail.com>
2019-11-07 16:31:19 +0200

    cor

--- lesson4/hw2/index.js
+++ lesson4/hw2/index.js
@@ -5,12 +5,13 @@ let result = 0;
 for (let i = m; i <= n; i++) {
     if ((i % 2 === 0) && (i % 4 !== 0)) {
         result += i;
-    } else if (i % 3 !== 0) {
+    } else if (i % 3 === 0) {
         result -= i;
-    } else if (i % 4 !== 0) {
+    } else if (i % 4 === 0) {
         result *= i;
-    } else if (i % 5 !== 0) {
+    } else if (i % 5 === 0) {
         console.log(i);
         continue;
     }
 }
+console.log("Result: " + result);
