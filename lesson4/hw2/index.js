aa66491760a4d8f9c4f15d40a2e8438c91efcd4f
liubov4 <liub.chernova@gmail.com>
2019-11-07 14:25:50 +0200

    cor

--- lesson4/hw2/index.js
+++ lesson4/hw2/index.js
@@ -1,27 +1,18 @@
-const m = 0;
+const m = 1;
 const n = 15;
 let res = 1;
 
 for (let i = m; i <= n; i++) {
     if (i % 2 == 0 && i % 4) {
         res += i;
-    }
-}
-for (let i = m; i <= n; i++) {
-    if (i % 3) {
+    } else if (i % 3) {
         res -= i;
-    }
-}
-for (let i = m; i <= n; i++) {
-    if (i % 4) {
+    } else if (i % 4) {
         res *= i;
-    }
-}
-for (let i = m; i <= n; i++) {
-    if (i % 5) {
+    } else if (i % 5) {
         console.log(i);
     } else {
         continue;
     }
+    console.log(res);
 }
-console.log("Result: ", +res);
